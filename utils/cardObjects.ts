120934b06b64e3147124b10c26226d37f930fcd8
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-12-25 18:54:08 +0530

    `Added flipped property to Card interface and populated it for all card objects`

--- utils/cardObjects.ts
+++ utils/cardObjects.ts
@@ -1,5 +1,3 @@
-
-
 const red="#D32F2F";
 const green = "#04db48";
 const blue = "#0381FF";
@@ -9,290 +7,347 @@ interface Card{
     color: string,
     value: string,
     id: string,
+    flipped: boolean
 }
 
 const red1 : Card = {
     color: red,
     value: '1',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red2 : Card = {
     color: red,
     value: '2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red3 : Card = {
     color: red,
     value: '3',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red4 : Card = {
     color: red,
     value: '4',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red5 : Card = {
     color: red,
     value: '5',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red6 : Card = {
     color: red,
     value: '6',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red7 : Card = {
     color: red,
     value: '7',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red8 : Card = {
     color: red,
     value: '8',
-    id: ''
+    id: '',
+    flipped: false
 }
 const red9 : Card = {
     color: red,
     value: '9',
-    id: ''
+    id: '',
+    flipped: false
 }
 const redSkip : Card = {
     color: red,
     value: 'S',
-    id: ''
+    id: '',
+    flipped: false
 }
 const redReverse : Card = {
     color: red,
     value: 'R',
-    id: ''
+    id: '',
+    flipped: false
 }
 const redDraw2 : Card = {
     color: red,
     value: '+2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const redWild : Card = {
     color: red,
     value: 'Wild',
-    id: ''
+    id: '',
+    flipped: false
 }
 const redWildDraw4 : Card = {
     color: red,
     value: '+4',
-    id: ''
+    id: '',
+    flipped: false
 }
 
 const blue1 : Card = {
     color: blue,
     value: '1',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue2 : Card = {
     color: blue,
     value: '2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue3 : Card = {
     color: blue,
     value: '3',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue4 : Card = {
     color: blue,
     value: '4',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue5 : Card = {
     color: blue,
     value: '5',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue6 : Card = {
     color: blue,
     value: '6',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue7 : Card = {
     color: blue,
     value: '7',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue8 : Card = {
     color: blue,
     value: '8',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blue9 : Card = {
     color: blue,
     value: '9',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blueSkip : Card = {
     color: blue,
     value: 'S',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blueReverse : Card = {
     color: blue,
     value: 'R',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blueDraw2 : Card = {
     color: blue,
     value: '+2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blueWild : Card = {
     color: blue,
     value: 'Wild',
-    id: ''
+    id: '',
+    flipped: false
 }
 const blueWildDraw4 : Card = {
     color: blue,
     value: '+4',
-    id: ''
+    id: '',
+    flipped: false
 }
 
 const green1 : Card = {
     color: green,
     value: '1',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green2 : Card = {
     color: green,
     value: '2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green3 : Card = {
     color: green,
     value: '3',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green4 : Card = {
     color: green,
     value: '4',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green5 : Card = {
     color: green,
     value: '5',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green6 : Card = {
     color: green,
     value: '6',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green7 : Card = {
     color: green,
     value: '7',
-    id: ''
+    id: '',
+    flipped: false
 }   
 const green8 : Card = {
     color: green,
     value: '8',
-    id: ''
+    id: '',
+    flipped: false
 }
 const green9 : Card = {
     color: green,
     value: '9',
-    id: ''
+    id: '',
+    flipped: false
 }
 const greenSkip : Card = {
     color: green,
     value: 'S',
-    id: ''
+    id: '',
+    flipped: false
 }
 const greenReverse : Card = {
     color: green,
     value: 'R',
-    id: ''
+    id: '',
+    flipped: false
 }
 const greenDraw2 : Card = {
     color: green,
     value: '+2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const greenWild : Card = {
     color: green,
     value: 'Wild',
-    id: ''
+    id: '',
+    flipped: false
 }
 const greenWildDraw4 : Card = {
     color: green,
     value: '+4',
-    id: ''
+    id: '',
+    flipped: false
 }
 
 const yellow1 : Card = {
     color: yellow,
     value: '1',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow2 : Card = {
     color: yellow,
     value: '2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow3 : Card = {
     color: yellow,
     value: '3',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow4 : Card = {
     color: yellow,
     value: '4',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow5 : Card = {
     color: yellow,
     value: '5',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow6 : Card = {
     color: yellow,
     value: '6',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow7 : Card = {
     color: yellow,
     value: '7',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow8 : Card = {
     color: yellow,
     value: '8',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellow9 : Card = {
     color: yellow,
     value: '9',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellowSkip : Card = {
     color: yellow,
     value: 'S',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellowReverse : Card = {
     color: yellow,
     value: 'R',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellowDraw2 : Card = {
     color: yellow,
     value: '+2',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellowWild : Card = {
     color: yellow,
     value: 'Wild',
-    id: ''
+    id: '',
+    flipped: false
 }
 const yellowWildDraw4 : Card = {
     color: yellow,
     value: '+4',
-    id: ''
+    id: '',
+    flipped: false
 }
 
 const cardList = [red1, red2, red3, red4, red5, red6, red7, red8, red9, blue1, blue2, blue3, blue4, blue5, blue6, blue7, blue8, blue9, green1, green2, green3, green4, green5, green6, green7, green8, green9, yellow1, yellow2, yellow3, yellow4, yellow5, yellow6, yellow7, yellow8, yellow9, redDraw2, redSkip, redReverse, redWildDraw4, blueDraw2, blueSkip, blueReverse, blueWildDraw4, greenDraw2, greenSkip, greenReverse, greenWildDraw4, yellowDraw2, yellowSkip, yellowReverse, yellowWildDraw4]
